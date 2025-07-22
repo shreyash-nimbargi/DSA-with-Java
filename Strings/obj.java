0b763cd41a98fcc80ca0236d4ef9d089a80ddce2
shreyash-nimbargi <shreenim27@gmail.com>
2025-07-22 19:52:46 +0530

    Added String file

---
+++ Strings/obj.java
@@ -0,0 +1,12 @@
+public class obj {
+    public static void check()
+    {
+        String a = "Shreyash";
+        String b = "Shreyash";
+        System.out.println(a==b);
+    }
+    public static void main(String args[])
+    {
+        check();
+    }
+}
