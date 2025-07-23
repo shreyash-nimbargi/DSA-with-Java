7434c90bf8023245ebb28b31cf1abe97a23169f5
shreyash-nimbargi <shreenim27@gmail.com>
2025-07-23 20:38:02 +0530

    Added secondLargest number Program

--- Array/Largest.java
+++ Array/Largest.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 public class Largest {
   
-    public  static void manual()
+    public  static void manual(int[] arr)
     {
       
       Arrays.sort(arr);
       System.out.println(Arrays.toString(arr));
-      int largest = arr[arr.length-2];
-      System.out.println(largest);
+      int big = arr[arr.length-1];
+      System.out.println(big);
+    }
+
+    static void secondlargest(int arr[])
+    {
+        int sLar =  arr[arr.length-2];
+        System.out.println(sLar);
     }
     public static void main(String[] args) {
       int[] arr = {23,34,54,67};
-      manual();
+      manual(arr);
+      secondlargest(arr);
   }
 }
