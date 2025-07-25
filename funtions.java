553e1a2ca1e442e16db74afa320b6d2ca43d8733
shreyash-nimbargi <shreenim27@gmail.com>
2025-07-25 22:03:19 +0530

    Added charAt function

--- funtions.java
+++ funtions.java
@@ -6,5 +6,11 @@ public class funtions {
         String lastName = "Nimbargi";
         String fullName = firstName + " " +lastName;
         System.out.println(fullName);
+        System.out.println(fullName.length());
+
+        for(int i = 0;i<fullName.length();i++)
+        {
+            System.out.print(fullName.charAt(i));
+        }
     }
 }
