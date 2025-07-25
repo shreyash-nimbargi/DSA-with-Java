78699ef1975589dd64ceddfd9a7648b20fe5abf2
shreyash-nimbargi <shreenim27@gmail.com>
2025-07-25 21:43:17 +0530

    Added String program

---
+++ funtions.java
@@ -0,0 +1,10 @@
+
+
+public class funtions {
+    public static void main(String[] args) {
+        String firstName = "Shreyash";
+        String lastName = "Nimbargi";
+        String fullName = firstName + " " +lastName;
+        System.out.println(fullName);
+    }
+}
