2b1ec2b877bd9369a5dde0dee32fe2873c2283c7
shreyash-nimbargi <shreenim27@gmail.com>
2025-08-03 17:49:12 +0530

    quick update

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -1,29 +1,67 @@
 
 
 public class LL {
+    Node head;
 
+    public LL()
+    {
+        this.head = null;
+    }
+        
     private class Node{
-        String data;
+        int data;
         Node next;
 
-        Node(String data){
+        Node(int data){
             this.data = data;
             this.next = null;
         }
     }
 
-    public void addFirst(String data)
+    public  void addFirst(int  data)
     {
-        Node newNode = new Node(data);
+        Node newnode = new Node(data);
         if(head == null)
         {
-            head = newNode;
+            head = newnode;
             return;
         }
+        newnode.next = head;
+        head = newnode;
         
     }
-    public static void main(String[] args) {
-        LL list = new LL();
+    public void addLast(int data)
+    {
+        Node lastnode = new Node(data);
+        if(head == null)
+        {
+            head = lastnode;
+            return;
+        }
 
+        Node currNode = head;
+        while(currNode.next != null)
+        {
+            currNode = currNode.next;
+        }
+        currNode.next = lastnode;
+
+    }
+    public  void printList()
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        LL l = new LL();
+        l.addFirst(1);
+        l.addFirst(2);
+        l.addLast(3);
+        l.addLast(4);
+        l.printList();
     }
 }
