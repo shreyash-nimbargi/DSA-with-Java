ee9f427407b1077144d1e484d17c32f51a1dddd0
shreyash-nimbargi <shreenim27@gmail.com>
2025-08-03 13:36:38 +0530

    Added funtions

--- Recursion1.java
+++ Recursion1.java
@@ -1,20 +1,27 @@
 public class Recursion1 {
 
-    public static  void p1(int n)
-    {
-        if(n==5)
-        {
-            
-            return;
-        }
-        
-        p1(n + 1);
-            System.out.println(n);
+    
+    public static class Node{
+        int data;
+        Node next;
 
-        
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
     }
     public static void main(String[] args) {
-        
-        p1(1);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        n1.next = n2;
+        n2.next = n3;
+        Node temp = n1;
+        while(temp!= null)
+        {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
