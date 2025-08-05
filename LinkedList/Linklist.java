6849c778d2eea738291cb01058a0620e62a8427e
shreyash-nimbargi <shreenim27@gmail.com>
2025-08-05 10:04:56 +0530

    quick update

--- LinkedList/Linklist.java
+++ LinkedList/Linklist.java
@@ -33,16 +33,67 @@ public class Linklist {
         head = newNode;
 
 
-        //print
+        
     }
+
+    public void addLast(int data)
+    {
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+            
+        }
+        temp.next = newNode;
+    }
+
+    //delete
+
+    public void deleteFirst()
+    {
+        if(head == null)
+        {
+            System.out.println("the list is empty");
+            return;
+        }
+        head = head.next;
+    }
+
+    //delete last
+
+    public void deleteLast()
+    {
+        if(head == null)
+        {
+            System.out.println("this list is empty");
+            return;
+        }
+
+        Node slast = head;
+        Node last = head.next;
+        while(last.next != null)
+        {
+            last = head.next;
+            slast = head;
+        }
+    }
+    //print the elements
+
     public void printList()
     {
         Node temp = head;
         while(temp != null)
         {
-            System.out.println(temp.data+" -> ");
+            System.out.print(temp.data+" -> ");
             temp = temp.next;
         }
+        System.out.print("NULL");
     }
 
     
@@ -51,7 +102,9 @@ public class Linklist {
         Linklist l = new Linklist();
         l.addFirst(2);
         l.addFirst(1);
-
+        l.addLast(3);
+        l.printList();
+        l.deleteFirst(2);
         l.printList();
     }
 }
